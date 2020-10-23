514506b0565f380caf717f2dc47cc526370d723f
serenapascual <serena.a.pascual@gmail.com>
2020-10-22 22:48:14 -0700

    Update to Swift 5

--- Podfile
+++
@@ -1,6 +0,0 @@
-platform :ios, '8.0'
-
-target 'mechmarket' do
-  use_frameworks!
-  pod 'OAuthSwift', '~> 1.1.0'
-end
---
+++ mechmarket/mechmarket/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  mechmarket
+//
+//  Created by Serena Pascual on 10/22/20.
+//  Copyright © 2020 serenapascual. All rights reserved.
+//
+
+import SwiftUI
+import OAuthSwift
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
